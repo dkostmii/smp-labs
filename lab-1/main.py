a2fd36a28f44ba106e1ac288d2c24ddbae7baae5
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-10-16 19:54:24 +0300

    Apply typing

--- lab-1/main.py
+++ lab-1/main.py
@@ -1,5 +1,6 @@
 from config.opts import read_opts
 from domain.state import CalculatorState
+from procedures import Procedure
 from procedures.calc import do_calculation_proc
 from procedures.config import change_config_proc
 from procedures.exit_proc import exit_proc
@@ -7,7 +8,7 @@ from procedures.history_proc import display_history_proc
 from procedures.memory import clear_memory_proc, save_to_memory_proc
 from std.read import read_choose_from_list, read_yes_no
 
-operations = {
+operations: dict[str, Procedure] = {
     "change_config": change_config_proc,
     "display_history": display_history_proc,
     "clear_memory": clear_memory_proc,
